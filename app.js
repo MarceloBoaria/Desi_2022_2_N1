1b4dd08a32739c720d0c4cd87c982f2778e6c150
Marcelo Boaria <marceloboaria@gmail.com>
2023-02-16 21:58:29 -0300

    Exemplos da aula do dia 16/02

---
+++ app.js
@@ -0,0 +1,13 @@
+let valor = 7
+
+function ConferindoValores() {
+
+    if (valor <= 5) {
+        console.log('menores');
+    } else {
+        console.log('maiores');
+    }
+
+}
+
+ConferindoValores()
